30979e7ca0f1cbb9887746828d5bd28c0e1a1052
Akash Srivastava <akashniki@gmail.com>
2024-02-25 02:46:55 +0100

    umi: parts: Switch to ParanoidDoze

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Doze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
